dd657daa29a101d335d1717bb8e4d0c200df104d
US <singhu12345890@gmail.com>
2025-04-30 11:56:59 +0530

    crud-live

--- frontend/studentapp/src/components/Update.jsx
+++ frontend/studentapp/src/components/Update.jsx
@@ -7,7 +7,7 @@ const Update = () => {
         const name = e.target.name.value
         const age = e.target.age.value
         const data={name,age}
-        await axios.put(`http://localhost:9000/users/${id}`,data)
+        await axios.put(`https://crud-live-backend-zj6q.onrender.com/users/${id}`,data)
         alert("Success")
     }
   
